296b8bf748e07cd7a3b68b2997de55dfed9c2f03
lifeslash <lifeslash.mail@gmail.com>
2019-06-02 15:54:49 +0900

    update

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# test code for Outlook
+
+This code accesses the contact information of your outlook.
+Before run this code, yolu have to install microsoft outlook.
+
+
+This code is rewriting for VB from C#, and the originak is <a href="https://codezine.jp/article/detail/1890?p=2">C#.NETによるMicrosoft Outlookの統合</a>
--- testOutlook/Form1.vb
+++ testOutlook/Form1.vb
@@ -2,9 +2,9 @@
 
 Public Class Form1
 
-    Public oApp As Outlook.Application
-    Public oNS As Outlook.NameSpace
-
+    Private oApp As Outlook.Application 'outlook application
+    Private oNS As Outlook.NameSpace    'outlook namespace, MAPI means parameter when use Namespaces
+    Private oContactsFolder As Outlook.MAPIFolder   '
 
     Private Sub form_laod(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
         oApp = New Outlook.Application()
@@ -13,6 +13,7 @@ Public Class Form1
     End Sub
 
     Private Sub Button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button1.Click
+        oContactsFolder = oNS.PickFolder()  'the return is object of MAPIFolder type
 
     End Sub
 
