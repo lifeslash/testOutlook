8b764463e136cf41bdf8c23c10449e02f6b43aa3
lifeslash <lifeslash.mail@gmail.com>
2019-06-02 15:40:49 +0900

    first commit

---
+++ testOutlook/Form1.vb
@@ -0,0 +1,19 @@
+﻿Imports Outlook = Microsoft.Office.Interop.Outlook
+
+Public Class Form1
+
+    Public oApp As Outlook.Application
+    Public oNS As Outlook.NameSpace
+
+
+    Private Sub form_laod(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        oApp = New Outlook.Application()
+        oNS = oApp.GetNamespace("MAPI")
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+End Class
